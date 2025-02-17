e1309d63cb51ed00c9e9bed6e7a18530034104eb
Chen-Lin Chu <chenlinchu637@gmail.com>
2025-02-17 22:50:16 +0800

    加入模板試玩按鈕

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 
 import Index from '../components/pages/index.vue';
@@ -59,7 +59,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
     scrollBehavior(_to, _from, savedPosition) {
         if (savedPosition) {
